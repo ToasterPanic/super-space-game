44b8a3ef8ca5bc6299b6be5afac90f6604cf7322
ToasterPanic <me@owouw.us>
2025-12-01 17:35:07 -0500

    Basic spaceship movement, better physics engine

---
+++ scripts/player.gd
@@ -0,0 +1,13 @@
+extends RigidBody2D
+
+var health = 1000
+
+func _process(delta: float) -> void:
+	var axis = Input.get_axis("turn_left", "turn_right")
+	
+	angular_velocity = deg_to_rad(180 * axis)
+	
+	var new_velocity = transform.y * Input.get_axis("forward", "backward") * 512
+	
+	if new_velocity.length() > linear_velocity.length() - 12:
+		linear_velocity = transform.y * Input.get_axis("forward", "backward") * 512
