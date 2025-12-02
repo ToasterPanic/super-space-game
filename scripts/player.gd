66ed42e3cbe9745b346d562f62bb4e9fe61e920c
ToasterPanic <me@owouw.us>
2025-12-01 19:10:54 -0500

    Asteroid collisions, camera shake, sound effects

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,7 +1,21 @@
 extends RigidBody2D
 
 var health = 1000
-var time_since_last_collision = 0
+var time_since_last_collision = 1
+var camera_shake_power = 0
+
+func _process(delta: float) -> void:
+	$Camera.offset.x = randi_range(-camera_shake_power, camera_shake_power)
+	$Camera.offset.y = randi_range(-camera_shake_power, camera_shake_power)
+	
+	if camera_shake_power > 0:
+		camera_shake_power -= delta * 20
+		
+	if camera_shake_power < 0:
+		camera_shake_power = 0
+	
+	if time_since_last_collision <= 1:
+		time_since_last_collision += delta
 
 func _physics_process(delta: float) -> void:
 	var axis = Input.get_axis("turn_left", "turn_right")
@@ -10,5 +24,17 @@ func _physics_process(delta: float) -> void:
 	
 	var new_velocity = transform.y * Input.get_axis("forward", "backward") * 512
 	
-	if new_velocity.length() > linear_velocity.length() - 12:
+	if (new_velocity.length() > linear_velocity.length() - 12) :
 		linear_velocity = transform.y * Input.get_axis("forward", "backward") * 512
+		if time_since_last_collision < 1:
+			linear_velocity *= (time_since_last_collision + 0.15) * 2
+
+
+func _on_hitbox_body_shape_entered(body_rid: RID, body: Node2D, body_shape_index: int, local_shape_index: int) -> void:
+	if body.linear_velocity.length() + linear_velocity.length() > 256:
+		health -= 100
+		camera_shake_power = 4 
+		
+		$Collision.play()
+		
+		time_since_last_collision = 0
