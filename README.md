5843f87a34db71ec1718f1ac20cdf43d4eef3286
ToasterPanic <me@owouw.us>
2025-12-02 17:04:12 -0500

    New sprites, remove Rapier2D because it's causing weird physics bugs

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Super Space Game
+
+A full remake of my previous Space Game with far more features, content and functionality. The only borrowed assets from the original game are specific sound effects.
--- scripts/player_ground.gd
+++ scripts/player_ground.gd
@@ -1,9 +1,24 @@
 extends CharacterBody2D
 
+func _ready() -> void:
+	$Sprite.play()
+
 func _process(delta: float) -> void:
 	var horizontial_movement = Input.get_axis("ground_left", "ground_right")
 	var vertical_movement = Input.get_axis("ground_up", "ground_down")
 	
 	velocity = Vector2(256 * horizontial_movement, 256 * vertical_movement)
+	if horizontial_movement > 0.1:
+		$Sprite.scale.x = -2
+	if horizontial_movement < -0.1:
+		$Sprite.scale.x = 2
+	
+	if velocity.length() > 8:
+		$Sprite.animation = "walk"
+		if abs(horizontial_movement) > abs(vertical_movement): $Sprite.speed_scale = abs(horizontial_movement)
+		else: $Sprite.speed_scale = abs(vertical_movement)
+	else:
+		$Sprite.animation = "idle"
+		
 	
 	move_and_slide()
