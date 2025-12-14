721b67392b7e5c62bc9ee34143a16d6b3db4fbad
ToasterPanic <me@owouw.us>
2025-12-14 17:48:29 -0500

    Fix title screen scaling, credits disclaimer, fix tutorial corner detection issue

--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -59,4 +59,4 @@ func _on_are_you_sure_back_pressed() -> void:
 func _on_yes_quit_to_title_pressed() -> void:
 	if visible:
 		get_tree().paused = false
-		get_tree().change_scene_to_file("res://scenes/title.tscn" )
+		get_tree().change_scene_to_file("res://scenes/title.tscn")
