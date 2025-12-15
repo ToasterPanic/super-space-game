a79a191c0b22ef39a7df404938fe46133dbb28d5
ToasterPanic <me@owouw.us>
2025-12-14 21:20:59 -0500

    Auto export version plugin & its configuration

---
+++ addons/AutoExportVersion/auto_export_version_config_file.gd
@@ -0,0 +1,15 @@
+extends "res://addons/AutoExportVersion/VersionProvider.gd"
+
+func get_version(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> String:
+	var version: String = ""
+	
+	var time = Time.get_datetime_dict_from_system()
+	version = str(time.year) + "." + str(time.month).pad_zeros(2) + str(time.day).pad_zeros(2) + "." + str(get_git_commit_count()) + "-" + get_git_branch_name()
+
+#	version += get_git_commit_count()
+#	version += get_git_branch_name()
+#	version += get_git_commit_hash()
+#	version += get_export_preset_version()
+#	version += get_export_preset_android_version_code() + " " + get_export_preset_android_version_name()
+	
+	return version
